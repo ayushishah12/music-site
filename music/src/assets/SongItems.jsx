42521439b4cf199ba22dcb9a1026aa2380445d7a
Ayushishah@12 <ayu12072004@gmail.com>
2024-09-26 19:16:15 +0530

    changes

---
+++ music/src/assets/SongItems.jsx
@@ -0,0 +1,8 @@
+
+
+
+<div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+<img className='rounded'src={image}/>
+<p className='font-bold mt-2 md-1'>{name}</p>
+<p className='text-slate-200 text-sm'>{desc}</p>
+</div>
